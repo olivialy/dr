832c38ee970196bd752bebd161dc891c123b61a5
olivialy <olivia_ly@yahoo.fr>
2017-07-07 22:57:37 +0200

    Set project structure

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+var gulp        = require('gulp'),
+    concat      = require('gulp-concat'),
+    cssnano      = require('gulp-cssnano'),
+    fs          = require('fs'),
+    glob        = require('glob'),
+    path        = require('path'),
+    plumber     = require('gulp-plumber'),
+    rename      = require('gulp-rename'),
+    rev         = require('gulp-rev'),
+    runSequence = require('run-sequence'),
+    stylus      = require('gulp-stylus'),
+    uglify      = require('gulp-uglify'),
+    uncss       = require('gulp-uncss');
+
+var cfg = {
+    bowerDir: 'vendor/bower_components/',
+    nodeDir: 'node_modules/',
+    assetsDir: 'assets/',
+    revManifestPath: 'assets/rev-manifest.json',
+    webDir: 'web/',
+    stylusPattern: 'styl/**/*.styl',
+    jsPattern: 'js/**/*.js'
+};
+
+// styles
+gulp.task('styles', function()
+{
+    return gulp.src(cfg.assetsDir + 'styl/otis.styl')
+        .pipe(plumber())
+        .pipe(stylus({
+            linenos: false,
+        }))
+        //.pipe(uncss({
+        //    html: ['**.html']
+        //}))
+        //.pipe(cssnano())
+        .pipe(gulp.dest(cfg.webDir + 'css/'));
+});
+
+// js
+gulp.task('scripts', function()
+{
+    return gulp.src([
+        cfg.assetsDir + 'js/otis.js'
+        ])
+        .pipe(plumber())
+        .pipe(concat('otis.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(cfg.webDir + 'js/'));
+});
+
+// clean
+gulp.task('clean', function() {
+    var files = glob.sync("web/@(js|css)/otis-*.*");
+    for (var file in files) {
+        fs.unlinkSync(files[file]);
+    }
+});
+
+// assets
+gulp.task('assets', function() {
+    // Retrieve html5shiv web/ directory
+    gulp.src(cfg.nodeDir + '/html5shiv/dist/html5shiv.min.js')
+        .pipe(gulp.dest('web/js'));
+
+});
+
+// build
+gulp.task('build', function(cb) {
+    runSequence(
+        ['clean', 'assets'],
+        ['styles', 'scripts'],
+        cb
+    );
+});
+
+// watch
+gulp.task('watch', function() {
+    gulp.watch(cfg.assetsDir +'/'+ cfg.stylusPattern, ['styles']);
+    gulp.watch(cfg.assetsDir +'/'+ cfg.jsPattern, ['scripts']);
+});
+
+// default
+gulp.task('default', ['build', 'watch']);
+
